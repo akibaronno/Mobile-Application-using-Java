58de880ececfcadc40b81d809afc780bee62853a
Farhad Ali Akib <165318583+akibaronno@users.noreply.github.com>
2025-10-16 21:29:49 +0600

    Add main class with favourite food example

---
+++ 4 different ways /main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Pasta myPasta = new Pasta();
+        myPasta.setFavouriteFood("Pasta");
+        System.out.println("My favourite food is: " + myPasta.getFavouriteFood());
+    }
+}
